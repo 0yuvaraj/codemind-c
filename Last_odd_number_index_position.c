79f93c2579a9a88050064ecd76a862ad71d8c73b
0yuvaraj <149353867+0yuvaraj@users.noreply.github.com>
2023-12-19 10:50:34 +0530

    #include <stdio.h> int main() { 	int i,f=0,x,y=0,z=0; 	scanf("%d ",&x); 	int a[x]; 	for (i=0;i<x;i++) 	{ 		scanf("%d",&a[i]); 	} 	for (i=0;i<x;i++) 	{ 		if (a[i]%2!=0) 		{ 			y=i; 		} 	} 	printf("%d",y); }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int i,f=0,x,y=0,z=0;
+	scanf("%d
+",&x);
+	int a[x];
+	for (i=0;i<x;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for (i=0;i<x;i++)
+	{
+		if (a[i]%2!=0)
+		{
+			y=i;
+		}
+	}
+	printf("%d",y);
+}
