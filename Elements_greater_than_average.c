07b90d54e7a897a2492585d293e2f6f55c7ebeb5
0yuvaraj <149353867+0yuvaraj@users.noreply.github.com>
2023-12-19 11:28:06 +0530

    #include <stdio.h> int main() { 	int i,f=0,x,y=0,z=0; 	scanf("%d ",&x); 	int a[x]; 	for (i=0;i<x;i++) 	{ 		scanf("%d",&a[i]); 	} 	for (i=0;i<x;i++) 	{ 	    y=y+a[i]; 	} 	for (i=0;i<x;i++) 	{ 		if (a[i]>=y/x) 		{ 			f++; 		} 	} 	printf("%d",f); }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int i,f=0,x,y=0,z=0;
+	scanf("%d
+",&x);
+	int a[x];
+	for (i=0;i<x;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for (i=0;i<x;i++)
+	{
+	    y=y+a[i];
+	}
+	for (i=0;i<x;i++)
+	{
+		if (a[i]>=y/x)
+		{
+			f++;
+		}
+	}
+	printf("%d",f);
+}
