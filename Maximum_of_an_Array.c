ba575e0834814a1122158d43d8d62776e9a43cd4
0yuvaraj <149353867+0yuvaraj@users.noreply.github.com>
2023-12-19 09:42:13 +0530

    #include <stdio.h> int main() { 	int i,f,y=0,x; 	scanf("%d ",&x); 	int a[x]; 	for (i=0;i<x;i++) 	{ 		scanf("%d",&a[i]); 	} 	y=a[0]; 	for (int j=0;j<x;j++) 	{ 		if (a[j]>y) 		{ 			y=a[j]; 		} 	} 	printf("%d",y); }

---
+++ Maximum_of_an_Array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int i,f,y=0,x;
+	scanf("%d
+",&x);
+	int a[x];
+	for (i=0;i<x;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	y=a[0];
+	for (int j=0;j<x;j++)
+	{
+		if (a[j]>y)
+		{
+			y=a[j];
+		}
+	}
+	printf("%d",y);	
+}
